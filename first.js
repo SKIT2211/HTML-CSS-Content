81a7756e3e7cc611f4adb0434a80a780cabb522f
SKIT2211 <shyamkasundra2001@gmail.com>
2023-03-06 12:06:15 +0530

    html code push

---
+++ first.js
@@ -0,0 +1,45 @@
+// var a = "hello";
+// console.log(a);
+
+// let qwe = prompt('Hello Guys! type Your Query here!!');
+// console.log(qwe);
+// let age = prompt("enter your age!")
+// let b = age<18 ? "You  are not eligible":"You're eligible for driving."
+// console.log(b);
+// document.getElementById("basic").innerHTML = 2323;
+
+// document.write(66);
+// let n = 10;
+// let space = "";
+
+// for (let a = 1; a <= n; a++) {
+//     for (let b = 1; b <= a; b++) {
+//         space += b;
+//     }
+//     space += "\n";
+// }
+// console.log(space);
+
+// let a = "shyam";
+// console.log(a);
+// let b = a.replace("shyam","Ram");
+// console.log(b);
+
+// let x = prompt("enter 1st numbers here");
+// let y = prompt("enter 2nd numbers here");
+// let z = aassdd(x,y);
+// function aassdd(x,y) {
+
+//     return (+x + +y);
+// }
+// console.log(( "your number of total is " + z));
+
+
+// const data = {
+//     name:"ram",
+//     id:122,
+//     age:15,
+//     address:"india"
+// }
+// console.log(data);
+
